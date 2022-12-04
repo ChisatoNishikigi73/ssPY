799f761d7ece7c52e5fa5f69bd6fe60bca7ca225
Kawaa-qwq <lxr2061895177@163.com>
2022-12-04 22:53:55 +0800

    fix pic name is identified as dir name

--- www.pivix.net爬虫.py
+++ www.pivix.net爬虫.py
@@ -14,19 +14,19 @@ from utils.DownloadUtils import download_pic, download_pic_without_hualihushao
 # 请先按照以下几步设置基本参数
 # 1，在24行填写您的pivix cookie
 # 2，在29行填写保存图片的目录
-# 3，设置249行参数
+# 3，设置206行参数
 # 如果要按照用户id爬取图片：mode="author", value=画师的id
 # 4，没了，还没做好
 
 headers = {
     'Referer': 'https://www.pixiv.net/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62',
-    'Cookie': '',
+    'Cookie': 'first_visit_datetime_pc=2022-05-08+16:48:57; p_ab_id=0; p_ab_id_2=7; p_ab_d_id=1116159261; yuid_b=OHGGVWY; privacy_policy_notification=0; adr_id=ftBSrPBkeYAMZgKjFrS862op0o4DgcQF6o231s6mSvXJPcTP; login_ever=yes; __utmz=235335808.1653750096.3.2.utmcsr=accounts.pixiv.net|utmccn=(referral)|utmcmd=referral|utmcct=/; cto_bundle=5Kklbl9VVDhUQlhGN0Z4VnVkM3pKaHdxalc3cmMlMkZCYjhtVllRbDVHaWtQbXE5M3B3aUJ0S2tmSGVjY1VVeVk3ZEk1QVYyWkp5ajhkTTM5ZGwzSXNCNEVCUlZJJTJGNlUxJTJCY1RZdFh5WnljbjNiN1JvU1ZES2JYZlN1aTlva3RwVk9zSm9jRWpGMWRXa29HbTdqV1IyQ29aOXBNaUElM0QlM0Q; __utmc=235335808; a_type=1; __utmv=235335808.|2=login ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=82169731=1^9=p_ab_id=0=1^10=p_ab_id_2=7=1^11=lang=zh=1; __utma=235335808.1226528258.1651996155.1658312947.1658312961.5; _ga=GA1.2.1226528258.1651996155; _ga_75BBYNYN9J=GS1.1.1658318869.2.1.1658320344.0; device_token=60a02e91e9471ae7ac56fde4dd827ce0; QSI_S_ZN_5hF4My7Ad6VNNAi=v:0:0; p_b_type=1; PHPSESSID=88138202_guW2QV5JWKbpUUXa6DPqg4h3nQTvKhnn; privacy_policy_agreement=5; c_type=22; b_type=0; __cf_bm=WlbS55t2WIDT.DY4xCO0RAIVSlCz52kWfkPhEpof0NU-1670131410-0-AXwVRRb/V6SydxTC7Squ61uQpQrn4TAm1ofdbmZoj5Fkg4dHyNehhYvxqvgHxvFSkBIUpwwU0/rnIqPGDkJMep4AypZefFo0FqLNie5CeMV2r6BWBNb0UYDeXY6AL5wusymdQ0452AcFDgixM5Cd3yNlVsgUvINt/WHve/5H27GLUU62xsETcGW8WF7+8HSMEvdnNbaDtCI5gsmBYccMpuU=; tag_view_ranking=vm3O8ATOGG~bL1ApaFBTu~Jxg8TkZQdK~RTJMXD26Ak~ZNRc-RnkNl~nWC-P2-9TI~q1r4Vd8vYK~zyKU3Q5L4C~8buMDtT-ku~j3leh4reoN~E6USWSAVMi~Ie2c51_4Sp~EA1nr9Hm18~PiKFMvIHS1~wC-YDaMscU~a7FYtDkb3F~wioq3KHpEK~9QhWZjeOmv~U9A9K0M8Oi~bqWQiQeNku~CRliPr1uKR~YJa1AYRvMw~DxtiQJj6de~vuzltsubQd~KvAGITxIxH~98c-9jH-Jp~dDVC9t_E1h~lH5YZxnbfC~dUhrZMpRPB~5H5jwYRKk2~QZh6FpjOHZ~Gcv5xjGZY3~d_iiHeHbdZ~zZSmJjkoF7~4ZEPYJhfGu~Mg6bq-SpX8~wBkXLfToxH~jMzRcTuF76~Lt-oEicbBr~jpIZPQ502H~Pk1xsLC9nL~pac41t5DOn~xx7VVwZNwa~IsmCxf13RP~K4vd3ajDHN~Ib0YGS-7FI~gHhGuI3hqJ~YGP_o2g7RI~Qa2ijP_TT3~8e-OBkgs0n~HY55MqmzzQ~MSNRmMUDgC~sOHNr0eXHD',
     'accept-language': 'ja-JP,jp;q=0.9'
 }
 
 pyid = 3
-path = ""
+path = "C:/Users/lxr20/Pictures/Camera Roll"
 
 
 def is_used_pages(raw_code, a):  # 判断是否是可用的网页
@@ -214,7 +214,11 @@ def mode_author(author_id, do_write_metadata):
                     f"({pic_ids[a]})正在下载{inf.get('information').get('userName')}->"
                     f"{inf.get('information').get('alt')}({b}/{page})"
                 )
-                download_pic(dl[b], t_path, f"{inf.get('information').get('alt')}-{b+1}", headers)
+                p_name = f"{inf.get('information').get('alt')}-{b+1}"
+                p_name = p_name.replace('/', '_').replace('\\', '_').replace(':', '_').replace('*', '_').replace('?','_')\
+                    .replace('"', '_').replace('<', '_').replace('>', '_').replace('|', '_')
+
+                download_pic(dl[b], t_path, p_name, headers)
                 b = b + 1
 
             write_information(t_path, inf)
@@ -246,4 +250,4 @@ def getJsonKey(json_data):
 
 
 if __name__ == '__main__':
-    main(use_list=False, mode="author", value="", do_write_metadata=True)
+    main(use_list=False, mode="author", value="19037720", do_write_metadata=True)
